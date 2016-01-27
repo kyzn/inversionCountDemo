7203e622f2e3bd74740b063f3e5d1ca517ffc3a3
Kivanc Yazan <kivancyazan@gmail.com>
2016-01-27 11:46:01 -0800

    Implemented mergesort function

--- assign10/assign10.py
+++ assign10/assign10.py
@@ -1,3 +1,4 @@
+
 #Brute Force Inversion Counting
 def brute_count(A):
     count=0
@@ -6,8 +7,36 @@ def brute_count(A):
             if A[i]>A[j]:
                 count+=1
     return count
-                
-                
-                
-                
-                
+
+
+#Mergesort helper function:
+def merge(L,R):
+    result=[]
+    while len(L)>0 and len(R)>0:
+        if L[0]<=R[0]:
+            result.append(L[0])
+            L=L[1:]
+        else:
+            result.append(R[0])
+            R=R[1:]
+    while len(L)>0:
+        result.append(L[0])
+        L=L[1:]
+    while len(R)>0:
+        result.append(R[0])
+        R=R[1:]
+    return result
+
+
+#Mergesort function
+def mergesort(A):
+    if len(A)<=1:
+        return A
+    else:
+        middle=len(A)//2
+        left = A[:middle]
+        right= A[middle:]
+        left = mergesort(left)
+        right= mergesort(right)
+        result= merge(left,right)
+        return result

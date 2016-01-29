0aa1e90aafb26c3f84ee49a86eb6027e27de3158
Kivanc Yazan <kivancyazan@gmail.com>
2016-01-29 09:19:48 -0800

    Implemented inversion count by brute force

---
+++ assign11/assign11.py
@@ -0,0 +1,8 @@
+#Count inversions by brute force
+def brute_count(A):
+    count = 0
+    for i in range(0,len(A)):
+        for j in range(i,len(A)):
+            if (A[i]>A[j]):
+                count+=1
+    return count

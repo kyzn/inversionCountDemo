020bb91fa09a532ba80e6003488bfc94a5180f8b
Kivanc Yazan <kivancyazan@gmail.com>
2016-01-29 10:09:18 -0800

    Implemented merge and mergesort

--- assign11/assign11.py
+++ assign11/assign11.py
@@ -7,3 +7,46 @@ def brute_count(A):
                 count+=1
     return count
     
+#Mergesort helper function
+def merge(L,R):
+    result = []
+    
+    while len(L)>0 and len(R)>0:
+        if (L[0]<=R[0]):
+            result.append(L[0])
+            L=L[1:]
+        else:
+            result.append(R[0])
+            R=R[1:]
+    
+    while len(L)>0:
+        result.append(L[0])
+        L=L[1:]    
+
+    while len(R)>0:
+        result.append(R[0])
+        R=R[1:]
+        
+    return result
+    
+    
+    
+#Mergesort
+def mergesort(A):
+    if len(A)<=1:
+        return A
+    else:
+        mid= len(A)//2
+        L = A[:mid]
+        R = A[mid:]
+        
+        L = mergesort(L)
+        R = mergesort(R)
+        result = merge(L,R)
+    return result
+        
+        
+        
+        
+        
+    
